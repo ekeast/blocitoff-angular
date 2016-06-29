08e9ff9ae5bf5cf630a7a275949c6a214648bd1d
Ted Keast <ekeast328@gmail.com>
2016-06-29 11:00:51 -0400

    Checkpoint 2 Completed

---
+++ dist/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,20 @@
+(function() {
+    function HomeCtrl($scope, $firebaseArray) {
+        this.heroTitle = "Welcome to Blocitoff!";
+        
+        var ref = new Firebase("https://blocitoff-angular-76381.firebaseio.com/");
+        $scope.tasks = $firebaseArray(ref);
+        
+        
+        $scope.addTask = function() {
+            $scope.tasks.$add({
+                text: "text"
+            });
+        };
+        
+    }
+    
+    angular
+        .module('blocitoff')
+        .controller('HomeCtrl',['$scope', '$firebaseArray', HomeCtrl]);
+})();
