084dd3d8047051c829f5b72297d4f9c733c715ee
ekeast <ekeast328@gmail.com>
2016-07-06 21:42:40 -0400

    Input Fixed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,6 @@
 (function() {
     function HomeCtrl($scope, $firebaseArray) {
         this.heroTitle = "Welcome to Blocitoff!";
-
         
         var ref = new Firebase("https://blocitoff-angular-76381.firebaseio.com/tasks");
         $scope.tasks = $firebaseArray(ref);
@@ -13,11 +12,16 @@
         
         $scope.addTask = function(newTask) {
             $scope.tasks.$add(newTask);
-            $scope.newTask = {};
+            $scope.newTask = {
+                text: '',
+                completed: false,
+                dateCreated: Firebase.ServerValue.TIMESTAMP
+            };
         };
         
         $scope.completeTask = function(task) {
             task.completed = true;
+            $scope.tasks.$save(task);
         };
         
         $scope.now = moment();
